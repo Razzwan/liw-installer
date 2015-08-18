2f38aba497f70407cdb96bcafa89efe8edf9f627
Razzwan <razvanlomov@gmail.com>
2015-08-18 13:03:50 +0300

    adf

--- Installer.php
+++ Installer.php
@@ -5,6 +5,6 @@ class Installer
 {
     static public function postCreateProject()
     {
-        mkdir(__DIR__ . '/newDir');
+        echo 'Razzwan: Поздравляю! Вы успешно установили Liw Framework.';
     }
 }
