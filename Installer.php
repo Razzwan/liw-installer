922fc6f16ae5b6510d2fae59f464db909595566f
Razzwan <razvanlomov@gmail.com>
2015-08-22 17:18:55 +0300

    \n

--- Installer.php
+++ Installer.php
@@ -5,6 +5,6 @@ class Installer
 {
     static public function postCreateProject()
     {
-        echo 'Razzwan: Поздравляю! Вы успешно установили Liw Framework!\n';
+        echo "\x1b[32mRazzwan:\x1b[0m Поздравляю! Вы успешно установили Liw Framework!\r\n";
     }
 }
